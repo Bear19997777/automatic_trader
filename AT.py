8931be9139d081cb5e65dc63d52ab01038ee2b27
Bear19997777 <jerry880915@gmail.com>
2023-02-23 23:33:03 +0800

    atlogin

--- AT.py
+++ AT.py
@@ -6,13 +6,22 @@ from datetime import timezone
 import numpy as np
 from dotenv import load_dotenv
 import os 
-load_dotenv()
-api = sj.Shioaji(simulation=True)
-print(os.getenv("api_key"))
-print(os.getenv("secrect_key"))
-api.login(
-    api_key=f'{os.getenv("api_key")}',
-    secret_key=f'{os.getenv("secrect_key")}'
+class at:
+    def __init__   (self):
+        load_dotenv()
+        api = sj.Shioaji(simulation=True)
+        self.__api = api
+        
+    def login(self):
+        self.__api.login(
+            api_key=f'{os.getenv("api_key")}',
+            secret_key=f'{os.getenv("secrect_key")}'
+            )
           
-)
+    def main(self):
+        self.login()
+if __name__ == "__main__":
+    atobj = at()
+    atobj.main()
+
 
